71d8781869055b45a704d22480e1fb23a4d5fcbb
laura.abukauskaite@visma.com <laura.abukauskaite@visma.com>
2017-07-07 15:10:55 +0300

    - FirstWeekProject 4 day, 2 code

---
+++ FirstWeekProject/Scripts/app.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("ContactApp", []);
+
+app.controller("ContactListController", function($http){
+    var self = this;
+    $http.get("/api/contacts").then(function (response) {
+        self.contacts = response.data;
+
+    });
+});
